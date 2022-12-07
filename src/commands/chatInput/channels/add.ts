c59ba258b1062524d6c1d5c8304e5c4418d27840
Talal <talalahmad257@gmail.com>
2022-12-07 10:22:20 +0000

    Finish add subcommand

--- src/commands/chatInput/channels/add.ts
+++ src/commands/chatInput/channels/add.ts
@@ -1,4 +1,5 @@
-import { ApplicationCommandOptionType } from "discord.js";
+import { ApplicationCommandOptionType, PermissionFlagsBits } from "discord.js";
+import ChannelModel from "../../../database/channels";
 import type{ ChatInputSubcommand } from "..";
 
 export default {
@@ -13,6 +14,34 @@ export default {
     },
   ],
   async execute(interaction) {
-    await interaction.reply("This command is not yet implemented");
+    const channel = interaction.options.getChannel("channel", true);
+    const doc = await ChannelModel.findOne({ guildId: interaction.guildId });
+    if (!channel.isTextBased() || channel.isDMBased() || channel.isThread()) {
+      return void interaction.reply("❌ You can only add text channels to the ticket system");
+    }
+    if (
+      !channel
+        .permissionsFor(interaction.guild.members.me!, true)
+        .has([PermissionFlagsBits.CreatePublicThreads, PermissionFlagsBits.CreatePrivateThreads])
+    ) {
+      return void interaction.reply(
+        "⚠ I need permissions to create public and private threads in that channel",
+      );
+    }
+    await interaction.deferReply();
+    if (!doc) {
+      const newDoc = new ChannelModel({
+        guildId: interaction.guildId,
+        channels: [channel.id],
+      });
+      await newDoc.save();
+    } else if (doc.channels.includes(channel.id)) {
+      return void interaction.editReply(`⚠ <#${channel.id}> is already in the ticket system`);
+    } else {
+      doc.channels.push(channel.id);
+      await doc.save();
+    }
+
+    return void interaction.editReply(`✅ Added <#${channel.id}> to the ticket system`);
   },
 } as ChatInputSubcommand;
