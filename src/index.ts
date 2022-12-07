7c196efe480c0081fd6827e340c832a42f886026
Talal <talalahmad257@gmail.com>
2022-12-07 11:55:06 +0000

    Increase GuildMember cache

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const client = new Client({
     GuildEmojiManager: 0,
     GuildForumThreadManager: 0,
     GuildInviteManager: 0,
-    GuildMemberManager: 0,
+    GuildMemberManager: 1,
     GuildScheduledEventManager: 0,
     GuildStickerManager: 0,
     GuildStickerPackManager: 0,
